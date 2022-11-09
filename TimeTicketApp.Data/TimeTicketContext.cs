0c140028bfad0fa76775d1d851e011f393fe906e
Evan Bush <EvanDBush.E.Bush05@gmail.com>
2022-11-09 15:38:07 -0500

    builds project class

--- TimeTicketApp.Data/TimeTicketContext.cs
+++ TimeTicketApp.Data/TimeTicketContext.cs
@@ -10,14 +10,16 @@ namespace TimeTicketApp.Data
         public DbSet<Employee>  Employees { get; set; }
         public DbSet<TimeTicket> TimeTickets { get; set; }
 
-        public string DbPath { get; }
+        public string DbPath { get; set; }
         public TimeTicketContext()
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "timeticket.db");
+            DbPath = System.IO.Path.Join(path, "timetickets.db");
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
     }
 }
---
+++ TimeTicketApp.Domain/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTicketApp.Domain
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+
+        public decimal Budget { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public List<TimeTicket> TimeTickets { get; set; } = new List<TimeTicket>();
+
+    }
+}

a069e57a7c878e0a078197c652a189df9515e3b5
Evan Bush <EvanDBush.E.Bush05@gmail.com>
2022-11-11 13:09:58 -0500

    tries function and view db update for EmployeeTimeTicket migration .failed

--- TimeTicketApp.Data/Migrations/20221111162521_EmployeeTimeTicketStats.cs
+++ TimeTicketApp.Data/Migrations/20221111162521_EmployeeTimeTicketStats.cs
@@ -10,12 +10,39 @@ namespace TimeTicketApp.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(
+                @"CREATE FUNCTION[dbo].[EarliestTimeTicketForEmployee](@EmployeeId int)
+                  RETURNS char(30) AS
+                  BEGIN
+                    DECLARE @ret char(30)
+                    SELECT TOP 1 @ret = DateTime
+                    FROM TimeTickets
+                    WHERE TimeTickets.TimeTicketId IN (SELECT TimeTicketId
+                                                       FROM TimeTicketEmployee
+                                                       WHERE EmployeeId = @employeeId)
+                    ORDER BY DateTime
+                    RETURN @ret
+                    END");
 
+            migrationBuilder.Sql(
+                @"CREATE VIEW dbo EmployeeTimeTicketStats
+                  AS
+                  SELECT dbo.Employees.Name
+                  COUNT (dbo.TimeTicketEmployee.TimeTicketId) AS NumberOfTimeTickets,
+                            dbo.EarliestTimeTicketForEmployee(MIN(dbo.Employees.Id))
+                       AS EarliestTimeTicket
+
+                  FROM dbo.TimeTicketEmployee INNER JOIN
+                        dbo.Employees ON dbo.TimeTicketEmployee.EmployeeId = dbo.Employees.Id
+                  GROUP BY dbo.Employees.Name, dbo.TimeTicketEmployee.EmployeeId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("DROP VIEW dbo.EmployeeTimeTicketStats");
+            migrationBuilder.Sql("DROP FUNCTION dbo.EarliestTimeTicketForEmployee");
+
 
         }
     }
